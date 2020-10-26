20cea119ab2cd3d075198229e3a031f2b2eff942
Ali Shahid <71405228+alishahid-github@users.noreply.github.com>
2020-10-26 06:55:39 +0500

    Create Hangman.py

---
+++ Hangman.py
@@ -0,0 +1,21 @@
+"""
+Hangman
+"""
+# start 8.a
+listOfWords = [
+    'APPLE', 'BILBO', 'CHORUSED', 'DISIMAGINE',
+    'ENSURING', 'FORMALISING', 'GLITCHES', 'HARMINE',
+    'INDENTATION', 'JACKED', 'KALPACS', 'LAUNDRY',
+    'MASKED', 'NETTED', 'OXFORD', 'PARODY', 'QUOTIENTS',
+    'RACERS', 'SADNESS', 'THYREOID', 'UNDUE', 'VENT', 'WEDGED',
+    'XERIC', 'YOUTHHOOD', 'ZIFFS'
+]
+
+import random as r
+
+print("Random word is: " + r.choice(listOfWords))
+
+
+# end 8.a
+
+
